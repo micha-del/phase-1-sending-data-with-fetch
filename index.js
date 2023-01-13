91299fc55599c3e6501ae936d4672b34803e5de8
Michael Kiros <michael.kiros00@gmail.com>
2023-01-13 14:39:00 -0500

    commit tree f7df1b813a91db2c5b2dd2bb64714f6522037915

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(firstName, email) {
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            "name": firstName,
+            "email": email
+        })
+    }
+    return fetch("http://localhost:3000/users", configObj)
+    .then(res => res.json())
+    .then(newUser => document.body.innerHTML = newUser['id'])
+    .catch(error => document.body.innerHTML = error.message)
+}
+
+function addNewContact(newID){
+    document.getElementsByTagName("body")[0].innerHTML = newID
+    document.body.append(message)
+}
+submitData("name", "name@name.com")
